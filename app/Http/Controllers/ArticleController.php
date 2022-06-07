45cc730b16066955e6d4c332016fa050e49617eb
nizanhulq <nizandhiaulhaq@mail.ugm.ac.id>
2022-06-07 21:16:19 +0700

    initial commit

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        return Article::all();
+    }
+
+    public function show($id)
+    {
+        return Article::find($id);
+    }
+
+
+
+    public function store(Request $request)
+    {
+        $article = new Article;
+        $article->author = $request->author;
+        $article->title = $request->title;
+        $article->description = $request->description;
+        $article->url = $request->url;
+        $article->urlToImage = $request->urlToImage;
+        $article->publishedAt = $request->publishedAt;
+        $article->content = $request->content;
+        $article->save();
+        return "Data berhasil disimpan";
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article = Article::find($id);
+        $article->author = $request->author;
+        $article->title = $request->title;
+        $article->description = $request->description;
+        $article->url = $request->url;
+        $article->urlToImage = $request->urlToImage;
+        $article->publishedAt = $request->publishedAt;
+        $article->content = $request->content;
+        $article->save();
+        return "Data berhasil diupdate";
+    }
+
+    public function delete($id)
+    {
+        Article::findOrFail($id)->delete();
+        return "Data berhasil dihapus";
+    }
+}
